94f8dee34e33c72813718a46257050f41f700ba4
adimbrugh <haisamismail71@gmail.com>
2025-06-30 20:41:50 +0400

    Refactor complex queries to improve performance.

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,40 +1,60 @@
--- Retrieves full booking info with user, property, and payment details
+-- Initial: Retrieve all bookings with user, property, and payment details
 SELECT 
     b.booking_id,
     b.start_date,
     b.end_date,
     b.total_price,
-    b.status,
-    u.user_id,
     u.first_name,
     u.last_name,
     u.email,
-    p.property_id,
     p.name AS property_name,
     p.location,
-    pay.payment_id,
     pay.amount,
     pay.payment_date,
     pay.payment_method
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON pay.booking_id = b.booking_id;
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+ORDER BY b.created_at DESC;
 
 
 
--- Refactored version with selected columns and indexing in mind
+-- Initial: Retrieve all bookings with user, property, and payment details
+EXPLAIN ANALYZE
 SELECT 
     b.booking_id,
     b.start_date,
     b.end_date,
+    b.total_price,
     u.first_name,
+    u.last_name,
     u.email,
+    p.name AS property_name,
+    p.location,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+ORDER BY b.created_at DESC;
+
+
+
+-- Refactored version with indexes assumed and optimized fields
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.first_name || ' ' || u.last_name AS user_full_name,
     p.name AS property_name,
     pay.amount,
     pay.payment_method
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON pay.booking_id = b.booking_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
 ORDER BY b.created_at DESC;
